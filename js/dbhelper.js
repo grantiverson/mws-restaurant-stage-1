2f41b3a983c6c0cc31da99ac838bf6c07711da50
Grant Iverson <iversonga@gmail.com>
2018-05-14 02:53:50 -0400

    Added conditional to get JSON file from appropriate URL

--- js/dbhelper.js
+++ js/dbhelper.js
@@ -8,9 +8,12 @@ class DBHelper {
    * Change this to restaurants.json file location on your server.
    */
   static get DATABASE_URL() {
-    // const port = 8000 // Change this to your server port
-    // return `http://localhost:${port}/data/restaurants.json`;
-    return 'http://iverson.io/projects/restaurant-reviews/data/restaurants.json';
+    if (location.hostname === 'localhost') {
+      const port = 8000 // Change this to your server port
+      return `http://localhost:${port}/data/restaurants.json`;
+    } else {
+      return 'http://iverson.io/projects/restaurant-reviews/data/restaurants.json';
+    }
   }
 
   /**
